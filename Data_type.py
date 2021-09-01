477747e44b5a8bcbfa02d1d0ca72b9dc06da67f2
papa0601 <rapy0601@naver.com>
2021-09-01 10:34:51 +0900

    sch2

---
+++ Data_type.py
@@ -0,0 +1,36 @@
+'''
+DataType 자료형
+Integer 정수형
+Character 문자형
+List 리스트형
+Tuple 튜플형
+Set 세트형
+Dict 사전형
+
+cal 연산자
++ - * / 사칙연산자
+= 대입연산자
+== 대등연산자 (수학에서의 '=')
+< > <= >= 크다, 작다
+!= 아니다
+'''
+print(1+1)
+i = 8
+j = 6
+k = 4
+l = 6
+
+print(i+j)
+print(i- k)
+print(j * k)
+print(i / k)
+print(j == l)
+print(i == j)
+print(i > j)
+print(i < j)
+print(i >= k)
+print(i <= k)
+print(j >= l)
+print(j <= l)
+print(i != j)
+print(j != l)
